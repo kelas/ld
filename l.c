f28c02018726a508a5e6e7425d9150416042d34c
kelas <me@kel.as>
2021-04-03 13:39:07 +0200

    [~]

--- l.c
+++ l.c
@@ -2,10 +2,10 @@
 #define SHR(m) m^=*in_q,*in_q=((J)(m))>>63,m // assume shr(J) well-defined
 #ifdef __ARM_NEON
 Zin U cmi(span z,G m){uint8x16_t vm=vset1(m);uint8x16_t r0=cmpeq(z.i0,vm),r1=cmpeq(z.i1,vm),r2=cmpeq(z.i2,vm),r3=cmpeq(z.i3,vm);R mvmask(r0,r1,r2,r3);}
-Zin U fqm(span x,U*in_q){U qbts=cmi(x,'"');U m=vmull_p64(-1ULL,qbts);R SHR(m);}
+Zin U fqm(span x,U*in_q){U qbts=cmi(x,QT);U m=vmull_p64(-1ULL,qbts);R SHR(m);}
 #else
 Zin U cmi(span z,G m){__m256i b=vset1(m);U x=(I)mvmask(cmpeq(z.lo,b)),y=mvmask(cmpeq(z.hi,b));R(y<<32)|x;}
-Zin U fqm(span x,U*in_q){U qbts=cmi(x,'"');U m=_mm_cvtsi128_si64(_mm_clmulepi64_si128(_mm_set_epi64x(0ULL,qbts),_mm_set1_epi8(0xFF),0));R SHR(m);}
+Zin U fqm(span x,U*in_q){U qbts=cmi(x,QT);U m=_mm_cvtsi128_si64(_mm_clmulepi64_si128(_mm_set_epi64x(0ULL,qbts),_mm_set1_epi8(0xFF),0));R SHR(m);}
 #endif
 #define z(i) p[ba+i]=idx+ctz(b),b=b&(b-1);
 Zin I zip(I*p,I ba,I idx,U b){P(!b,ba)I n=popcnt(b),nxb=ba+n;N(8,z(i))Z(n>8,N(8,z(i+8)));Z(n>16,ba+=16;do{z(0)ba++;}W(b));R nxb;}
